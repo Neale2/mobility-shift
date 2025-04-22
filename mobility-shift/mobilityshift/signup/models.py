a2c9cd828e67d5b19a02eb0a853d5194fa78fdf6
Neale2 <arturo.neale@gmail.com>
2025-04-22 15:35:29 +1200

    Added model for user storage

--- mobility-shift/mobilityshift/signup/models.py
+++ mobility-shift/mobilityshift/signup/models.py
@@ -1,3 +1,18 @@
+import uuid
+import datetime
 from django.db import models
 
-# Create your models here.
+from django.db.models import UniqueConstraint # Constrains fields to unique values
+from django.db.models.functions import Lower # Returns lower cased value of field
+
+class User(models.Model):
+    """Model storing users."""
+    uuid = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    email = models.EmailField(max_length=320, help_text='Enter your email')
+    sign_up_time = models.DateTimeField(editable=False, default=(datetime.datetime.now))
+    age_group = models.CharField(choices=[("<13", "Less than 13"), ("13-17", "13 - 17"), ("18-24", "18 - 24"), ("25-34", "25 - 34"), ("35-44", "35 - 44"), ("45-64", "45 - 64"), (">65", "More than 65"), ("prefer_not", "Prefer not to say")], help_text='Select your age group')
+    gender = models.CharField(choices=[("male", "Male"), ("female", "Female"), ("other", "Other"), ("prefer_not", "Prefer not to say")], help_text='Select your gender group')
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.email
