b584ef5b97f739e045bef4881b99fa846905e0c3
Neale2 <arturo.neale@gmail.com>
2025-04-25 15:07:08 +1200

    Backported Django email system to signup email

--- mobility-shift/mobilityshift/main/views.py
+++ mobility-shift/mobilityshift/main/views.py
@@ -1,11 +1,10 @@
 import sys
 import os
 import uuid
-from bs4 import BeautifulSoup
 
-from importlib import resources
 from . import send_email
 
+from django.template.loader import get_template
 from django.shortcuts import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.utils.translation import gettext_lazy as _
@@ -23,20 +22,15 @@ def signup(request):
             #Checking if error in saving
             try:
                 user_uuid = str(uuid.uuid4())
+                template = get_template('signedup-email.html')
+                context = {
+                    'email': form.cleaned_data['email'],
+                    'user_uuid': user_uuid,
+                }
+        
+                html_body = template.render(context)
                 
-                html_content = resources.files('main.emailtemplates').joinpath('signedup.html').read_text(encoding='utf-8')
-                soup = BeautifulSoup(html_content, 'html.parser')
-                
-                for i in soup.find_all("variable"):
-                    if i["class"] == ['email']:
-                        i.name = "span"
-                        i.string = form.cleaned_data['email']
-                    elif i["class"] == ['a-unsub-here']:
-                        i.name = "a"
-                        i["href"] = f'http://127.0.0.1:8000/unsubscribe/{user_uuid}'
-                        i.string = "here"
-                
-                print(send_email.send_email(form.cleaned_data['email'], "Welcome to the Programme!", str(soup), user_uuid))
+                print(send_email.send_email(form.cleaned_data['email'], "Welcome to the Programme!", html_body, user_uuid))
                 data = User(email=form.cleaned_data['email'], age_group=form.cleaned_data['age_group'], gender=form.cleaned_data['gender'], uuid=user_uuid)
                 data.save()
                 return HttpResponseRedirect("confirm/")
