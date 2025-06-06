855b4d71402f46840bf99ba2818023f463d092f2
Neale2 <arturo.neale@gmail.com>
2025-06-06 17:59:22 +1200

    fix allowed hosts

--- mobility-shift/mobilityshift/mobilityshift/settings.py
+++ mobility-shift/mobilityshift/mobilityshift/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = django_secretkey()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1:8000"]
+ALLOWED_HOSTS = ["app.swapone.nz"]
 
 
 # Application definition
