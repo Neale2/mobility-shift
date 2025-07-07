92fbbfda29799974f37209c597d94b1f308dedf4
Neale2 <arturo.neale@gmail.com>
2025-07-07 21:59:59 +1200

    Add users.csv to daily data dump

--- mobility-shift/mobilityshift/main/tasks.py
+++ mobility-shift/mobilityshift/main/tasks.py
@@ -128,7 +128,96 @@ def make_spreadsheet():
 
     download_url = f"{blob_client.url}?{sas_token}"
     
-    email_response = send_email("arturo.neale@gmail.com", "Daily Database Dump", "Hi! Here's the database from today: " + download_url, 'N/A')
+    
+    #second link for users :(
+    all_data_list = [
+        [
+            'UUID',
+            'Age Group',
+            'Sign Up Time',
+            'Emissions Saved (g)',
+            'Commute Distance (m)',
+            'Primary Vehicle (emission factor)',
+            'Employer',
+            'Region',
+            'Email',
+            'Name',
+            'Logged this week?'
+
+        ]
+    ]
+    users = []
+    for user in User.objects.all():
+        users.append(user)
+    for user in DeletedUser.objects.all():
+        users.append(user)
+    for user in users:
+        try:
+            data = [
+                str(user.uuid),
+                user.age_group,
+                str(user.sign_up_time.strftime('%Y-%m-%d %H:%M:%S')),
+                user.emissions_saved,
+                str(user.distance),
+                str(user.vehicle),
+                user.employer,
+                user.region,
+                
+            ]
+            #deleted accounds don't have emails
+            try:
+                data.append(user.email)
+            except:
+                data.append("DELETED")
+            #deleted accounds don't have names
+            try:
+                data.append(user.name)
+            except:
+                data.append("DELETED")
+            #deleted accounts don't have trip logged
+            try:
+                data.append(user.logged_this_week)
+            except:
+                data.append("False")
+            
+        except Exception as e:
+            data=["ERROR WITH USER. ERROR CODE:", e]
+            
+        all_data_list.append(data)
+        
+    try:
+        csv_file_path = 'users.csv'
+        # Open the file in write mode
+        with open(csv_file_path, 'w', newline='') as file:
+            # Create a CSV writer object
+            writer = csv.writer(file, quoting=csv.QUOTE_MINIMAL)
+
+            # Write the data rows
+            writer.writerows(all_data_list)
+    except Exception as e:
+        print("CSV GEN ERROR:", e)
+    
+
+    #azure connect to storage
+    blob_service_client = BlobServiceClient.from_connection_string(azure_storage_connection_string())
+    blob_client = blob_service_client.get_blob_client(container=azure_spreadsheet_bucket_name(), blob="users.csv")
+
+    with open("users.csv", "rb") as data:
+        blob_client.upload_blob(data, overwrite=True)
+
+
+    sas_token = generate_blob_sas(
+        account_name=blob_service_client.account_name,
+        container_name=azure_spreadsheet_bucket_name(),
+        blob_name="users.csv",
+        account_key=blob_service_client.credential.account_key,
+        permission=BlobSasPermissions(read=True),
+        expiry=datetime.utcnow() + timedelta(days=1)
+    )
+
+    user_download_url = f"{blob_client.url}?{sas_token}"
+    
+    email_response = send_email("arturo.neale@gmail.com", "Daily Database Dump", "Hi! Here's the trips database from today: " + download_url + " And here's the users: " + user_download_url, 'N/A')
     print(email_response)
     
     
