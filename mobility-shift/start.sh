fef3b98cece11ab85a29e1262b389bd818ba3bfa
Neale2 <arturo.neale@gmail.com>
2025-09-08 21:26:44 +1200

    add better start that controls postgres

--- mobility-shift/start.sh
+++ mobility-shift/start.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-# Trap script termination to ensure both child processes are killed
+# Trap script termination to ensure child processes are killed
 trap 'kill $(jobs -p)' EXIT
 
-# Run collectstatic (non-interactive, no input prompt)
+# Start PostgreSQL in the background
+sudo systemctl start postgresql
+# Optional: wait a moment to ensure Postgres is up
+sleep 2
+
+# Run collectstatic
 python3.13 -m poetry run mobilityshift/manage.py collectstatic --noinput
 
 # Start the scheduler in the background
 python3.13 -m poetry run mobilityshift/manage.py runapscheduler &
 SCHEDULER_PID=$!
 
-# Start the Django server (gunicorn) in the foreground
+# Start Django (gunicorn) in the foreground
 python3.13 -m poetry run ./gunicorn.sh
 
-# When runserver exits, script will reach here and the trap will kill the scheduler
+# When gunicorn exits, trap will kill the scheduler
+# Stop PostgreSQL too
+sudo systemctl stop postgresql
